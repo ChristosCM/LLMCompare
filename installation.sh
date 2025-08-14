91f8ff573ba805158cf509695efbed9eca24eade
Chirstos Mavropoulos <you@example.com>
2025-08-14 11:41:50 +0100

    add installation and startup instructions for macos

--- installation.sh
+++ installation.sh
@@ -1,4 +1,4 @@
 # for macos specific 
-
-
-pip install gradio google-generativeai openai anthropic
+brew install python@3.11 git 
+git clone https://github.com/ChristosCM/LLMCompare.git
+python3.11 -m pip install gradio google-generativeai openai anthropic
---
+++ startup.sh
@@ -0,0 +1,4 @@
+export GOOGLE_API_KEY=""
+export OPENAI_API_KEY=""
+export ANTHROPIC_API_KEY=""
+python3.11 multi_prompt.py
